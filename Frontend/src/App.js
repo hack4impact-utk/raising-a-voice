8fef24d121bd0c6d8a2defe191c74ea77752c8a0
tflorey <60160829+tflorey@users.noreply.github.com>
2021-10-20 17:33:29 -0400

    Initial login page attempt with some mui components and hooks

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,9 +1,11 @@
 import './styles/App.css';
+import Login from './components/Login.js'
 
 function App() {
   return (
     <div className="App">
       Begin inserting your code here!
+      <Login></Login>
     </div>
   );
 }
